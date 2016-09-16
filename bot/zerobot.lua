383434a489a4074defd2eeacc9f34b733eb3f00c
Erfan Bozorgi <NeoErfan@users.noreply.github.com>
2016-09-16 16:56:27 +0430

    Zero Bot V1

--- bot/zerobot.lua
+++ bot/zerobot.lua
@@ -232,7 +232,7 @@ function create_config( )
 	"whitelist",
 	"msg_checks"
     },
-    sudo_users = {110626080,103649648,111020322,0,tonumber(our_id)},--Sudo users
+    sudo_users = {224661370,235384878,257261175},--Sudo users
     moderation = {data = 'data/moderation.json'},
     about_text = [[❤Zero Bot V1❤
 
